cadb5ff4cd79a4640c8f31e2dcb1a487a1c2204d
Mateusz-Musik <mateusz.musik1@gmail.com>
2024-05-15 17:25:30 +0200

    Zadanie domowe 2

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,5 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io/commands/actions)
+    cy.visit('https://www.edu.goit.global/account/login')
   })
 })
